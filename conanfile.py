41f41ef98c60a49c8433c5fbafbfeb8753a2d93b
Emmanuel Prunet <emmanuel.prunet@coderoast.fr>
2026-06-30 10:47:28 +0200

    1.7.0 start

--- conanfile.py
+++ conanfile.py
@@ -25,7 +25,7 @@ class CodeRoastIpcConan(ConanFile):
     """
 
     name = "coderoast_ipc"
-    version = "1.6.5"
+    version = "1.7.0"
     package_type = "header-library"
     license = "Apache-2.0"
     description = (
